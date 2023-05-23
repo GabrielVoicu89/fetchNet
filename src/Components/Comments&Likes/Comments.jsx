c279929385e5ecece339652b468ab11ef68af000
GabrielVoicu89 <g_the_reaper@yahoo.com>
2023-05-23 13:28:45 +0200

    comments done no css
    Co-authored-by: Malekbennasser <Malekbennasser@users.noreply.github.com>

---
+++ src/Components/Comments&Likes/Comments.jsx
@@ -0,0 +1,3 @@
+export default function Comments(props) {
+  return <p>{props.comment}</p>;
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import Posts from "../../Components/Posts/Posts";
 import Navbar from "../../assets/Nav/Navbar";
-import CreatePost from "../../Components/CreatePost/CreatePost";
+
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <CreatePost />
       <Posts />
     </div>
   );
